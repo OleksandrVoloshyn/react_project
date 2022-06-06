4ae74d32bde5cfe7b8d3157b1709cd45e7cd0f07
OleksandrVoloshyn <oleksandr.vvn@gmail.com>
2022-06-06 14:16:31 +0300

    fix TS any

--- src/components/GenreBadge/GenreBadge.tsx
+++ src/components/GenreBadge/GenreBadge.tsx
@@ -1,31 +1,34 @@
 import React, {FC, useEffect} from "react"
 import {useSearchParams} from "react-router-dom";
-import {SubmitHandler, useForm} from "react-hook-form";
+import {useForm} from "react-hook-form";
 
 import {useAppDispatch, useAppSelector} from "../../hook";
 import {genreAction} from "../../redux";
 import css from './GenreBadge.module.css'
 
+type GenresForm = {
+    genresArray: string[]
+}
+
 const GenreBadge: FC = () => {
     const {allGenres} = useAppSelector(({genreReducer}) => genreReducer);
     const dispatch = useAppDispatch();
     const [query, setQuery] = useSearchParams({page: '1', with_genres: ''});
-    const {register, handleSubmit} = useForm();
+    const {register, handleSubmit} = useForm<GenresForm>();
 
     useEffect(() => {
         dispatch(genreAction.getAll())
     }, [dispatch])
 
-    const submit: SubmitHandler<any> = ({genresArray}): void => {
-    // const submit: SubmitHandler<{ genresArray: string[] }> = ({genresArray}): void => {
+    const submit = handleSubmit(({genresArray}) => {
         let queryObj = Object.fromEntries(query.entries());
         queryObj.with_genres = genresArray.toString()
         setQuery(queryObj)
-    }
+    })
 
     return (
         <div>
-            <form onSubmit={handleSubmit(submit)} className={css.filterForm}>
+            <form onSubmit={submit} className={css.filterForm}>
                 {allGenres.map(genre => <div key={genre.id}>
                     <label>{genre.name}
                         <input type={"checkbox"} {...register('genresArray')} value={genre.id}/>
